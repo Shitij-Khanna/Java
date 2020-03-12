c01dfabd56b8f78c8c624992af64484620f84003
Shitij Khanna <Shitij.Khanna@bookspot.nl>
2020-03-12 11:32:14 +0100

    collections

--- MultiThreading_Java/src/com/study/threads/CallableTest.java
+++ MultiThreading_Java/src/com/study/threads/CallableTest.java
@@ -6,7 +6,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
  
 class MyMath {
-   public static int add(int a, int b) {
+   public static int add(int a, int b){
        return a + b;
    }
 }
@@ -27,7 +27,7 @@ public class CallableTest {
                    executor.submit(
 		        new Callable<Integer>() {
 			    public Integer call() {
-			        return MyMath.add(x, y);
+						return MyMath.add(x, y);
 			    }
 			});
 	
