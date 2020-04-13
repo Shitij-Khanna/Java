8ceb3172d5d16d753f468f00b0180eb4497d6c33
Shitij Khanna <Shitij.Khanna@bookspot.nl>
2020-04-13 14:47:09 +0200

    linked lists,  threading

---
+++ DSA/src/com/practice/datastructures/linkedlist/CircularSinglyLinkedList.java
@@ -0,0 +1,5 @@
+package com.practice.datastructures.linkedlist;
+
+public class CircularSinglyLinkedList {
+
+}
--- DSA/src/com/practice/datastructures/linkedlist/LinkedList.java
+++ DSA/src/com/practice/datastructures/linkedlist/LinkedList.java
@@ -1,4 +1,4 @@
-package com.practice.datastructures;
+package com.practice.datastructures.linkedlist;
 
 public class LinkedList {
 
